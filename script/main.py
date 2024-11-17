8e7b65119a18537d2bd88eafdddde549912c95eb
Lars-Kuehn <kuehn.lars@icloud.com>
2024-11-18 00:53:57 +0100

    User can now change source- & destination-path

--- script/main.py
+++ script/main.py
@@ -1,5 +1,3 @@
-""" This module contains all the logic involving the DeskSave GUI and Algorithm"""
-
 import os
 import getpass
 import datetime
@@ -8,6 +6,7 @@ import shutil
 import sys
 import tkinter as tk
 from tkinter import messagebox, ttk, filedialog
+from datetime import datetime
 
 class DeskSaveApp(tk.Tk): # pylint: disable=too-many-instance-attributes
     """
@@ -43,10 +42,19 @@ class DeskSaveApp(tk.Tk): # pylint: disable=too-many-instance-attributes
 
         # Set up source options
         self.user = getpass.getuser()
-        self.allowed_sources = {
+        self.allowed_sources = { # Default sources
             'Desktop': f'/Users/{self.user}/Desktop',
             'Downloads': f'/Users/{self.user}/Downloads'
         }
+
+        # Initialize custom destination path
+        self.custom_destination_path = None
+
+        # Get the current month and year for the default destination path
+        self.current_month_year = datetime.now().strftime("%m-%Y")
+
+        # Set the default destination path to be inside the user's Documents/DeskSave folder, with the month-year subfolder
+        self.default_destination_path = os.path.join(f'/Users/{self.user}/Documents', 'DeskSave', self.current_month_year)
 
         # Create Menus
         self.create_menus()
@@ -69,8 +77,16 @@ class DeskSaveApp(tk.Tk): # pylint: disable=too-many-instance-attributes
         config_menu.add_command(label="Remove Custom Config", command=self.remove_custom_config)
         config_menu.add_command(label="About Config Syntax", command=self.about_custom_config)
 
-        # Add Config submenu to Settings
-        settings_menu.add_cascade(label="Config", menu=config_menu)
+
+        # Path Submenu
+        path_menu = tk.Menu(settings_menu, tearoff=0)
+        path_menu.add_command(label="Add Source-Path", command=self.add_custom_source)
+        path_menu.add_command(label="Change Destination-Path", command=self.change_destination)
+        path_menu.add_command(label="About Changing Paths", command=self.about_paths)
+
+        # Add Submenus to Settings
+        settings_menu.add_cascade(label="File Configuration", menu=config_menu)
+        settings_menu.add_cascade(label="Path Configuration", menu=path_menu)
 
         # Add Settings to Menu Bar
         menu_bar.add_cascade(label="Settings", menu=settings_menu)
@@ -181,113 +197,149 @@ class DeskSaveApp(tk.Tk): # pylint: disable=too-many-instance-attributes
         )
         close_button.pack(pady=10)
 
+    def add_custom_source(self):
+        """
+        Allows the user to add a custom source directory to the list of allowed sources.
+        """
+        # Prompt the user to select a folder
+        new_source_path = filedialog.askdirectory(title="Select a New Source Folder")
+        if new_source_path:
+            source_name = os.path.basename(new_source_path)  # Use the folder name as the key
+            if new_source_path in self.allowed_sources.values():
+                messagebox.showinfo("Source Path Exists", "This source path is already listed.")
+            else:
+                self.allowed_sources[source_name] = new_source_path
+                self.log_progress(f"Source '{source_name}' has been added successfully!")
+                messagebox.showinfo("Source Added", f"Source '{source_name}' has been added successfully!")
+
+                # Update the source dropdown after adding the new source
+                self.update_source_dropdown()
+
+    def change_destination(self):
+        """
+        Allows the user to specify a custom destination folder for organizing files.
+        Adds '/DeskSave' at the end of the selected destination folder.
+        """
+        # Prompt the user to select a folder
+        new_destination_path = filedialog.askdirectory(title="Select a Destination Folder")
+        if new_destination_path:
+            # Append /DeskSave to the selected folder path
+            new_destination_path = os.path.join(new_destination_path, "DeskSave", self.current_month_year)
+            
+            # Set the custom destination path
+            self.custom_destination_path = new_destination_path
+            
+            # Ensure the destination directory exists
+            os.makedirs(self.custom_destination_path, exist_ok=True)
+
+            self.log_progress(f"Destination path has been set to: {new_destination_path}")
+            messagebox.showinfo("Destination Changed", f"Destination path has been set to:\n{new_destination_path}")
+        else:
+            messagebox.showinfo("Destination Not Changed", "No destination path was selected.")
+
+
+    def about_paths(self):
+        """
+        Displays information about configuring custom paths and lists the default paths.
+        """
+        about_message = (
+            "You can specify custom source and destination paths.\n\n"
+            "Default Paths:\n"
+            "1. Source Paths (where your files are located):\n"
+            f"   - Desktop: {self.allowed_sources['Desktop']}\n"
+            f"   - Downloads: {self.allowed_sources['Downloads']}\n"
+            "2. Destination Path (where the sorted files will be moved to):\n"
+            f"   - Documents folder\n\n"
+            "You can modify these paths to suit your preferences. Ensure that the paths are correctly set to avoid file loss."
+        )
+        messagebox.showinfo("About Path Configuration", about_message)
+
     def show_about(self):
         """
-        Displays information about the application, including its version.
+        Displays information about the DeskSave application.
         """
-        app_version = os.getenv("APP_VERSION", "NO-STABLE-RELEASE")
-        messagebox.showinfo(
-            "About DeskSave",
-            f"DeskSave {app_version}\n\nAn application to organize your files effortlessly!"
+        about_message = (
+            "DeskSave Application\n\n"
+            "Version 1.0\n"
+            "A simple file organization tool for sorting files into predefined categories based on their extensions.\n"
+            "Developed by [Your Name]."
         )
+        messagebox.showinfo("About DeskSave", about_message)
+
+    def create_widgets(self):
+        """
+        Creates the main widgets for the GUI.
+        """
+        # Source dropdown
+        self.source_label = tk.Label(self, text="Select Source Folder:", font=("Arial", 14), fg="white", bg="#1e1e1e")
+        self.source_label.pack(pady=10)
+
+        # Display the full paths in the dropdown
+        self.source_combobox = ttk.Combobox(self, values=list(self.allowed_sources.values()), state="readonly")
+        self.source_combobox.pack(pady=10)
+
+        # Start Button
+        self.start_button = tk.Button(self, text="Start Organizing", command=self.organize_files, bg="#1e90ff", fg="black", font=("Arial", 14))
+        self.start_button.pack(pady=20)
+
+        # Progress Text Box
+        self.progress_text_box = tk.Text(self, height=10, width=50, state=tk.DISABLED, bg="#252526", fg="white", font=("Courier", 12))
+        self.progress_text_box.pack(pady=10)
 
     def load_file_types(self):
         """
-        Loads file type configurations from the active JSON configuration file.
-        If a custom JSON file is not provided, it defaults to the original 'file_types.json'.
-
-        Returns:
-            dict: A dictionary where keys represent file type categories (e.g., 'Images', 
-            'Documents') and values are lists of associated file extensions.
-
-        Raises:
-            SystemExit: If the active configuration file is missing or has an invalid format.
+        Loads the file types from the default JSON configuration file.
         """
-        active_path = self.custom_json_path or self.default_json_path
         try:
-            with open(active_path, 'r', encoding='UTF-8') as file:
+            with open(self.default_json_path, 'r', encoding='UTF-8') as file:
                 return json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
-            messagebox.showerror("Error", "Failed to load file types configuration.")
+        except FileNotFoundError:
+            messagebox.showerror("Error", "Configuration file not found.")
             sys.exit()
-    
-    def create_widgets(self):
-        """
-        Creates and arranges the graphical user interface (GUI) components for the DeskSaveApp.
+        except json.JSONDecodeError:
+            messagebox.showerror("Error", "Configuration file is invalid.")
+            sys.exit()
 
-        This includes setting up labels, dropdowns, buttons, and a text area to display
-        progress. The GUI components allow the user to select a source folder, start
-        the file organization process, and view a log of actions taken during execution.
-
-        Components:
-            - Title label displaying the application name.
-            - Dropdown menu for selecting the source folder (Desktop or Downloads).
-            - Organize button to initiate the file organization.
-            - Progress text area to show real-time updates on files being moved or skipped.
-        """
-        # Title Label
-        title_label = tk.Label(self, text="DeskSave", font=("Arial", 24), fg="#1e90ff", bg="#1e1e1e")
-        title_label.pack(pady=20)
-        
-        # Source Selection
-        source_label = tk.Label(self, text="Choose source folder to organize:", fg="white", bg="#1e1e1e")
-        source_label.pack()
-        
-        # Dropdown for source selection
-        self.source_var = tk.StringVar(value="Desktop")
-        source_menu = ttk.Combobox(self, textvariable=self.source_var, values=list(self.allowed_sources.keys()), state="readonly")
-        source_menu.pack(pady=10)
-        
-        # Organize Button
-        organize_button = tk.Button(self, text="Organize Files", command=self.organize_files, bg="#1e90ff", fg="black", font=("Arial", 14))
-        organize_button.pack(pady=20)
-        
-        # Progress Display
-        self.progress_text = tk.Text(self, width=70, height=10, bg="#252526", fg="white", state="disabled")
-        self.progress_text.pack(pady=10)
-        
     def log_progress(self, message):
         """
-        Logs a message to the progress text area within the GUI.
-
-        Args:
-            message (str): The message to display in the progress text area.
-        
-        Side Effects:
-            Updates the progress text area with the provided message, automatically
-            scrolling to the latest entry.
+        Logs progress in the progress text box.
         """
-        self.progress_text.config(state="normal")
-        self.progress_text.insert(tk.END, f"{message}\n")
-        self.progress_text.config(state="disabled")
-        self.progress_text.see(tk.END)
-        
+        self.progress_text_box.config(state=tk.NORMAL)
+        self.progress_text_box.insert(tk.END, f"{message}\n")
+        self.progress_text_box.config(state=tk.DISABLED)
+
+    def update_source_dropdown(self):
+        """
+        Updates the source dropdown menu after a new source is added.
+        """
+        self.source_combobox['values'] = list(self.allowed_sources.values())
+        self.source_combobox.set(list(self.allowed_sources.values())[-1])
+
     def organize_files(self):
         """
-        Handles the file organization process based on the selected source folder.
-
-        This method retrieves the user's choice from the dropdown menu, constructs a
-        destination path within the user's Documents folder, and defines files and
-        folders to skip. Then, it calls `move_files` to categorize and move files
-        according to the loaded file type configurations.
-
-        Side Effects:
-            - Displays progress messages in the text area.
-            - Shows an information dialog box upon completion.
+        Organizes files from the selected source folder into the specified categories based on their extensions.
         """
-        choice = self.source_var.get()
-        source = self.allowed_sources[choice]
+
+        # Set destination‚
+        destination = self.custom_destination_path or self.default_destination_path
+
+        # Ensure destination exists
+        os.makedirs(destination, exist_ok=True)
+
+        # Get the selected source folder from the dropdown
+        selected_source = self.source_combobox.get()
+
+
+        if selected_source:
+            self.log_progress(f"Organizing files from: {selected_source}")
+            skip_files = ['.DS_Store', 'README.md']
+            skip_folders = ['DeskSave', 'Downloads', 'Documents']
         
-        date = datetime.datetime.today().strftime("%Y-%m")
-        destination = f'/Users/{self.user}/Documents/OrganizedFiles/{date}/{choice}/'
-        
-        skip_files = ['.DS_Store', 'README.md']
-        skip_folders = ['DeskSave', 'Downloads', 'Documents']
-        
-        self.move_files(source, destination, self.file_types, skip_files, skip_folders)
-        self.log_progress("Organizing complete.")
-        messagebox.showinfo("Completed", "Organizing complete.")
-    
+            self.move_files(selected_source, destination, self.file_types, skip_files, skip_folders)
+            self.log_progress("Files have been organized successfully!")
+        else:
+            messagebox.showerror("Error", "Invalid source folder.")
+
     def move_files(self, source, destination, file_types, skip_files, skip_folders):
         """
         Moves and organizes files from the source directory to the destination directory,
@@ -297,37 +349,22 @@ class DeskSaveApp(tk.Tk): # pylint: disable=too-many-instance-attributes
         Files and folders can be excluded from the organization process based on the
         `skip_files` and `skip_folders` lists. After moving files, it attempts to delete
         any empty folders left in the source directory.
-
-        Args:
-            source (str): The directory to move files from.
-            destination (str): The directory to move files to, with subdirectories
-                organized by file type.
-            file_types (dict): A dictionary that maps file types to their extensions.
-            skip_files (list): List of file names or extensions to skip.
-            skip_folders (list): List of folder names to skip.
-
-        Side Effects:
-            - Creates destination folders if they do not already exist.
-            - Moves files and deletes empty folders in the source directory.
-            - Logs progress messages to the GUI text area.
-        
-        Raises:
-            OSError: If unable to delete an empty folder, logs an error message but
-            continues the process for other files and folders.
         """
-        if not os.path.exists(destination):
-            os.makedirs(destination)
         
         for item in os.listdir(source):
             full_path = os.path.join(source, item)
             
+            # Skip folders that are in the skip list
             if os.path.isdir(full_path) and item in skip_folders:
                 self.log_progress(f"Skipping folder: {item}")
                 continue
+            
+            # Skip files that are in the skip list or hidden files (e.g., .DS_Store)
             if item in skip_files or item.startswith('.'):
                 self.log_progress(f"Skipping file: {item}")
                 continue
             
+            # Process directories: move their contents to the appropriate file type folder
             if os.path.isdir(full_path):
                 folder_dest = os.path.join(destination, item)
                 if not os.path.exists(folder_dest):
@@ -342,6 +379,8 @@ class DeskSaveApp(tk.Tk): # pylint: disable=too-many-instance-attributes
                     self.log_progress(f"Deleted empty folder: {item}")
                 except OSError:
                     self.log_progress(f"Failed to delete folder {item} (it may not be empty)")
+            
+            # Process files: move them to the appropriate file type folder
             elif os.path.isfile(full_path):
                 file_extension = item.split('.')[-1].lower()
                 for file_type, data in file_types.items():
@@ -353,6 +392,6 @@ class DeskSaveApp(tk.Tk): # pylint: disable=too-many-instance-attributes
                         self.log_progress(f"Moved: {item} to {file_type_dest}")
                         break
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = DeskSaveApp()
     app.mainloop()
